0e3b5b376f06a678b26e5add20e691bfc83a6908
Mukesh Gupta <mukeshg@mindfiresolutions.com>
2019-01-09 19:53:58 +0530

    [Update]

---
+++ Task1/practice1.js
@@ -0,0 +1,16 @@
+function showresult()
+{
+    document.getElementById('f1').innerHTML = document.register.fname.value;
+    document.getElementById('f2').innerHTML = document.register.mname.value; 
+    document.getElementById('f3').innerHTML = document.register.lname.value;
+    document.getElementById('f5').innerHTML = document.register.code.value;
+    document.getElementById('f6').innerHTML = document.register.phno.value;
+    document.getElementById('f7').innerHTML = document.register.dob.value;
+    document.getElementById('f8').innerHTML = document.register.email.value;
+    document.getElementById('f9').innerHTML = document.register.qual.value;
+    document.getElementById('f10').innerHTML = document.register.addr.value;
+    document.getElementById('f11').innerHTML = document.register.about.value;
+    
+}
+
+
